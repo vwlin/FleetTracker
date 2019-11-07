f83fde2fb799a0207175dbb80d3296543b4b5eb9
MalcolmMiller <malcolmmiller21@gmail.com>
2019-11-07 17:31:26 -0500

    Add code to control GPS Chip Select pin

--- src/gps.h
+++ src/gps.h
@@ -7,10 +7,20 @@
 #define SAFEBOOT_SELECT              P6SEL
 #define SAFEBOOT_OUTPUT              P6OUT
 
+#define GPS_CS_DIRECTION             P2DIR
+#define GPS_CS_PIN                   BIT2
+#define GPS_CS_SELECT                P2SEL
+#define GPS_CS_OUTPUT                   P2OUT
+
 #define SET_SAFEBOOT_AS_GPIO         SAFEBOOT_SELECT &= ~SAFEBOOT_PIN
 #define SET_SAFEBOOT_AS_OUTPUT       SAFEBOOT_DIRECTION |= SAFEBOOT_PIN
 #define DRIVE_SAFEBOOT_PIN_HIGH      SAFEBOOT_OUTPUT |= SAFEBOOT_PIN
 #define DRIVE_SAFEBOOT_PIN_LOW       SAFEBOOT_OUTPUT &= SAFEBOOT_PIN
 #define TOGGLE_SAFEBOOT_PIN          SAFEBOOT_OUTPUT ^= SAFEBOOT_PIN
+
+#define SET_GPS_CS_AS_GPIO           GPS_CS_SELECT &= ~GPS_CS_PIN
+#define SET_GPS_CS_AS_OUTPUT         SAFEBOOT_DIRECTION |= GPS_CS_PIN
+#define SELECT_GPS_CS                GPS_CS_OUTPUT &= GPS_CS_PIN //drive LOW
+#define DESELECT_GPS_CS              GPS_CS_OUTPUT |= GPS_CS_PIN //drive HIGH
 
 void configureGPS();
