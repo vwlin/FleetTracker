ec0ea179115b7933a9b0c28ee7fce74a6f919169
MalcolmMiller <malcolmmiller21@gmail.com>
2019-11-21 17:27:18 -0500

    Getting time & date (see debugging vars)

--- src/main.c
+++ src/main.c
@@ -21,6 +21,9 @@
 //#define HOME_NODE
 //#define TEST
 
+volatile unsigned char payload[92];
+unsigned int year, month, day, hour, min, sec;
+
 void main(void){
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
@@ -35,7 +38,26 @@ void main(void){
     configure_ublox_poll(navPacket, 0x01, 0x07);
     unsigned char ublox_input_buffer[100];
     int i;
+
+
+    //clear input buffer
+    for(i = 0; i<100; i++){
+        ublox_input_buffer[i] = 0;
+    }
+
     while(1){
+
+        //debugging
+        for(i = 0; i<92; i++){
+            payload[i] = ublox_input_buffer[i + 6];
+        }
+        year = (int)(payload[4]) + (int)(payload[5] << 8);
+        month = (int)payload[6];
+        day = (int)payload[7];
+        hour = (int)payload[8] - 5; //subtract 5 to get our local time
+        min = (int)payload[9];
+        sec = (int)payload[10];
+
         SPI_SendPacket_GPS(configPacket, 28);
         while(ublox_input_buffer[0] == 0) {
             SPI_ReceivePacket_GPS(ublox_input_buffer,100);
