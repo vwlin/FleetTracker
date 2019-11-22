863bfef9b61ef6382da0ce2b75837bf02ae4d37b
Vivian <vwl4ac@virginia.edu>
2019-11-21 19:34:34 -0500

    updated timeout

--- src/protocol.h
+++ src/protocol.h
@@ -59,7 +59,7 @@ uint8_t Roamer_EstablishConnection(uint8_t * data, uint8_t size, uint8_t * start
  * DATA TRANSFER PROCEDURE
  */
 
-#define TIMEOUT_IN_SECONDS          1
+#define TIMEOUT_IN_SECONDS          .750
 #define TIMEOUT_VALUE               TIMEOUT_IN_SECONDS/.000015625
 #define GIVEUP                      4
 #define GIVEUP_TRANSMIT             4
